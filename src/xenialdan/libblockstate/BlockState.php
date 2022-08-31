72a29df600cb4fbd6e28e7ed02a60f43dad22af0
XenialDan <thebigsmilexd@gmail.com>
2022-08-31 03:40:33 +0200

    v0.1.2: Simplify BlockState::__toString(), add $strict parameter to replaceBlockStateValues()

--- src/xenialdan/libblockstate/BlockState.php
+++ src/xenialdan/libblockstate/BlockState.php
@@ -7,6 +7,7 @@ namespace xenialdan\libblockstate;
 use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\utils\InvalidBlockStateException;
+use pocketmine\nbt\NoSuchTagException;
 use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\StringTag;
@@ -43,13 +44,14 @@ class BlockState{
 	}
 
 	/**
-	 * @param array $states Keys are state names, values are new values
+	 * @param array                        $states Keys are state names, values are new values
+	 * @param bool                         $strict Whether to throw an exception if a state is not found
 	 *
-	 * @return BlockState
-	 * @throws BlockQueryParsingFailedException
-	 * @throws UnexpectedTagTypeException
+	 * @phpstan-param array<string, mixed> $states
+	 *
+	 * @throws BlockQueryParsingFailedException|UnexpectedTagTypeException|NoSuchTagException
 	 */
-	public function replaceBlockStateValues(array $states) : BlockState{
+	public function replaceBlockStateValues(array $states, bool $strict = true) : BlockState{
 		/** @var BlockStatesParser $blockStatesParser */
 		$blockStatesParser = BlockStatesParser::getInstance();
 		$newBlockStateCompound = clone $this->state->getBlockState();
@@ -59,23 +61,25 @@ class BlockState{
 				$blockStateTag instanceof StringTag => $newBlockStateCompound->getCompoundTag("states")->setString($stateName, (string) $newValue),
 				$blockStateTag instanceof IntTag => $newBlockStateCompound->getCompoundTag("states")->setInt($stateName, (int) $newValue),
 				$blockStateTag instanceof ByteTag => $newBlockStateCompound->getCompoundTag("states")->setByte($stateName, (int) $newValue),
+				$blockStateTag === null && $strict => throw new NoSuchTagException("Tag $stateName not found"),
 				default => throw new UnexpectedTagTypeException("Unexpected tag type")
 			};
 		}
 		return $blockStatesParser->getFromCompound($newBlockStateCompound);
 	}
 
+	/**
+	 * @throws InvalidBlockStateException|UnexpectedTagTypeException
+	 */
 	public function __toString() : string{
 		$r = $this->state->getId();
 		$s = [];
 		foreach($this->state->getBlockState()->getCompoundTag("states") as $tagName => $tag){
-			if($tag instanceof StringTag || $tag instanceof IntTag){
-				$s[] = "$tagName=" . $tag->getValue();
-			}else if($tag instanceof ByteTag){
-				$s[] = "$tagName=" . ($tag->getValue() === 1 ? "true" : "false");
-			}else{
-				throw new InvalidBlockStateException("Unknown tag of type " . get_class($tag) . " detected");
-			}
+			$s[] = match (true) {
+				$tag instanceof StringTag, $tag instanceof IntTag => $tagName . "=" . $tag->getValue(),
+				$tag instanceof ByteTag => $tagName . "=" . ($tag->getValue() === 1 ? "true" : "false"),
+				default => throw new InvalidBlockStateException("Invalid block state detected for block " . $this->state->getId() . " with tag " . $tagName . " of type " . get_class($tag) . " only StringTag, IntTag and ByteTag are allowed")
+			};
 		}
 		if(count($s) > 0) $r .= '[' . implode(',', $s) . ']';
 		return $r;
